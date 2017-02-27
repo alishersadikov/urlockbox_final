e13a5016e8e458dcc24e87504f1e5444df618d79
Alisher Sadikov <alishersadikov@gmail.com>
2017-02-27 09:52:05 -0700

    add pass conf to user params in users controller

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -22,19 +22,20 @@ feature "user visits sign up path" do
     fill_in "user[password]", with: "password2"
     fill_in "user[password_confirmation]", with: "password2"
     click_on "Submit"
+    save_and_open_page
 
     expect(page).to have_content("This email has been used!")
-  # end
-  #
-  # scenario "signs up with non-matching passwords" do
-  #
-  #   visit new_user_path
-  #
-  #   fill_in "user[email]", with: "test@example.com"
-  #   fill_in "user[password]", with: "password1"
-  #   fill_in "user[password_confirmation]", with: "password2"
-  #   click_on "Submit"
-  #
-  #   expect(page).to have_content("Password mismatch!")
+  end
+
+  scenario "signs up with non-matching passwords" do
+
+    visit signup_path
+
+    fill_in "user[email]", with: "test@example.com"
+    fill_in "user[password]", with: "password1"
+    fill_in "user[password_confirmation]", with: "password2"
+    click_on "Submit"
+
+    expect(page).to have_content("Password mismatch!")
   end
 end

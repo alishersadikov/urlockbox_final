5055da9ae79697d2eb1f799110fe89159341dc83
Alisher Sadikov <alishersadikov@gmail.com>
2017-02-27 10:10:15 -0700

    add url validation to link

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,16 @@
 class Link < ApplicationRecord
+  belongs_to :user
+
+  validates :url, presence: true
+  validates :title, presence: true
+
+   def valid_url?(url)
+     uri = URI.parse(url)
+     uri.is_a?(URI::HTTP) && !uri.host.nil?
+   rescue URI::InvalidURIError
+     false
+   end
+
   scope :hot, -> {
     select('links.url as url')
       .joins('join reads on reads.link_id = links.id')

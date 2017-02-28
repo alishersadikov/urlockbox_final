4203ac5717fd71cffd96af7465bd1285e8dc5469
Alisher Sadikov <alishersadikov@gmail.com>
2017-02-27 19:20:48 -0700

    add color differentiation by read status

--- app/services/hotreads_service.rb
+++ app/services/hotreads_service.rb
@@ -5,16 +5,17 @@ class HotreadsService
   end
 
   def self.get_hotreads
-    conn = Faraday.new(url: "https://fast-island-59356.herokuapp.com/")
-    response = conn.get '/api/v1/reads'
-    hotreads = JSON.parse(response.body, symbolize_names: true)
-    hotreads.each do |hotread|
-      link = Link.find_by(url: hotread[:url])
-      if Read.find_by(link_id: link.id)
-        Read.find_by(link_id: link.id).update_attributes(count: hotread[:hit_count])
-      else
-        Read.create(link: link, count: hotread[:hit_count])
-      end
-    end
+  #   conn = Faraday.new(url: "https://fast-island-59356.herokuapp.com/")
+  #   response = conn.get '/api/v1/reads'
+  #   hotreads = JSON.parse(response.body, symbolize_names: true)
+  #   hotreads.each do |hotread|
+  #     link = Link.find_by(url: hotread[:url])
+  #     if Read.find_by(link_id: link.id)
+  #       Read.find_by(link_id: link.id).update_attributes(count: hotread[:hit_count])
+  #     else
+  #       Read.create(link: link, count: hotread[:hit_count])
+  #     end
+  #   end
+  ""
   end
 end

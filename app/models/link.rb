b4e9702fffcc3cf2bd2ad3831463ff23d99f9c6b
Alisher Sadikov <alishersadikov@gmail.com>
2017-02-27 17:18:54 -0700

    add hot & top link logic

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,6 @@
 class Link < ApplicationRecord
   belongs_to :user
+  has_many :reads
 
   validates :url, presence: true
   validates :title, presence: true
@@ -11,11 +12,20 @@ class Link < ApplicationRecord
      false
    end
 
-  scope :hot, -> {
-    select('links.url as url')
-      .joins('join reads on reads.link_id = links.id')
-      .where('reads.created_at > ?', Time.now - 1.day)
-      .group("links.url")
-      .order('count("reads".id) DESC').limit(10)
-  }
+   def hotread_status
+     all_reads = joins(:reads).order('count DESC').limit(10)
+     if allreads.first == self
+       "hot"
+     elsif all_reads.include?(self)
+       "top link"
+     end
+   end
+  # 
+  # scope :hot, -> {
+  #   select('links.url as url')
+  #     .joins('join reads on reads.link_id = links.id')
+  #     .where('reads.created_at > ?', Time.now - 1.day)
+  #     .group("links.url")
+  #     .order('count("reads") DESC').limit(10)
+  # }
 end
